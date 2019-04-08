f2633fcf42f4eba3a5d037425636fe9f96e0ad7b
Ikiru Yoshizaki <guitarrapc@users.noreply.github.com>
2019-04-09 02:48:28 +0900

    refactor: split tests

--- tests/UnityBuildRunner.Tests/UnitTest.cs
+++ tests/UnityBuildRunner.Tests/UnitTest.cs
@@ -102,48 +102,5 @@ namespace UnityBuildTunner.Tests
             var log = settings.GetLogFile(args);
             log.Should().Be(logfile);
         }
-
-        [Theory]
-        [InlineData(
-            "-----CompilerOutput:-stdout--exitcode: 1--compilationhadfailure: True--outfile: Temp/Assembly-CSharp.dll",
-            "DisplayProgressNotification: Build Failed",
-            "Error building Player because scripts had compiler errors",
-            @"2018-11-05T00:53:44.2566426Z DisplayProgressNotification: Build Failed
-        Error building Player because scripts had compiler errors
-        (Filename:  Line: -1)
-        Unloading 64 Unused Serialized files (Serialized files now loaded: 0)
-        System memory in use before: 63.0 MB.
-        System memory in use after: 63.4 MB.
-
-        Unloading 47 unused Assets to reduce memory usage. Loaded Objects now: 5728.
-        Total: 13.359500 ms (FindLiveObjects: 1.689200 ms CreateObjectMapping: 0.289900 ms MarkObjects: 11.349100 ms  DeleteObjects: 0.029600 ms)")]
-        public void ShouldThrowErrorFilter(params string[] inputs)
-        {
-            var builder = new Builder();
-            foreach (var input in inputs)
-            {
-                Assert.Throws<OperationCanceledException>(() => builder.ErrorFilter(input));
-            }
-        }
-
-        [Theory]
-        [InlineData(
-            "Unloading 64 Unused Serialized files (Serialized files now loaded: 0)",
-            "System memory in use before: 63.0 MB.", "DisplayProgressbar: Unity Package Manager")]
-        public void ShouldNotThrowErrorFilter(params string[] inputs)
-        {
-            var builder = new Builder();
-            foreach (var input in inputs)
-            {
-                try
-                {
-                    builder.ErrorFilter(input);
-                }
-                catch (Exception ex)
-                {
-                    Assert.True(false, ex.Message);
-                }
-            }
-        }
     }
 }
