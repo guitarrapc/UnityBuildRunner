3afbe793a88b585254e6476c93b50d3c23f9a622
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2023-06-27 04:56:23 +0900

    chore: update BuildGame

--- sandbox/Sandbox.Unity/Assets/Editor/BuildUnity.cs
+++ sandbox/Sandbox.Unity/Assets/Editor/BuildUnity.cs
@@ -1,25 +1,14 @@
 using UnityEditor;
-using System.Diagnostics;
-using System.IO;
 
 public class BuildUnity
 {
     [MenuItem("MyTools/Windows Build With Postprocess")]
     public static void BuildGame()
     {
-        // Get filename.
-        string path = EditorUtility.SaveFolderPanel("Choose Location of Built Game", "", "");
+        var path = "artifacts/BuiltGame.exe";
         string[] levels = new string[] { "Assets/Scenes/SampleScene.unity" };
 
         // Build player.
-        BuildPipeline.BuildPlayer(levels, path + "/BuiltGame.exe", BuildTarget.StandaloneWindows, BuildOptions.None);
-
-        // Copy a file from the project folder to the build folder, alongside the built game.
-        FileUtil.CopyFileOrDirectory("Assets/Templates/README.md", Path.Combine(path, "README.md"));
-
-        // Run the game (Process class from System.Diagnostics).
-        Process proc = new Process();
-        proc.StartInfo.FileName = path + "/BuiltGame.exe";
-        proc.Start();
+        BuildPipeline.BuildPlayer(levels, path, BuildTarget.StandaloneWindows, BuildOptions.None);
     }
 }
