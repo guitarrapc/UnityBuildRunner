f039522ddf16c7d4e385fe2aa4be5bc9092d9b94
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2024-06-25 12:56:32 +0900

    feat: scriptable build for Unity 2022.3.12

--- sandbox/Sandbox.Unity.IL2CPPDedicatedServer/Assets/Editor/BuildUnity.cs
+++ sandbox/Sandbox.Unity.IL2CPPDedicatedServer/Assets/Editor/BuildUnity.cs
@@ -1,52 +1,103 @@
 using UnityEditor;
+using UnityEditor.Build;
 
 public class BuildUnity
 {
     [MenuItem("MyTools/Build Windows IL2CPP Player")]
-    public static void BuildGame()
+    public static void BuildWindowsIl2cppPlayer()
     {
-        var path = $"artifacts/{PlayerSettings.productName}.il2cpp-player.exe";
-        string[] levels = new string[] { "Assets/Scenes/SampleScene.unity" };
+        var path = $"artifacts/Build.il2cpp-player.exe";
+        string[] scenes = new string[] { "Assets/Scenes/SampleScene.unity" };
 
-        // Build player.
+        var buildPlayerOptions = new BuildPlayerOptions();
+
+        // Platform & Architecture
         buildPlayerOptions.target = BuildTarget.StandaloneWindows;
         buildPlayerOptions.subtarget = (int)StandaloneBuildSubtarget.Player;
-        BuildPipeline.BuildPlayer(levels, path, BuildTarget.StandaloneWindows, BuildOptions.None);
+
+        // IL2CPP
+        EnableIl2Cpp(NamedBuildTarget.Standalone);
+
+        // Path and Scenes
+        buildPlayerOptions.locationPathName = path;
+        buildPlayerOptions.scenes = scenes;
+
+        // Build
+        BuildPipeline.BuildPlayer(buildPlayerOptions);
     }
 
     [MenuItem("MyTools/Build Windows IL2CPP DedicatedServer")]
-    public static void BuildGame()
+    public static void BuildWindowsIl2cppServer()
     {
-        var path = $"artifacts/{PlayerSettings.productName}.il2cpp-dedicatedserver.exe";
-        string[] levels = new string[] { "Assets/Scenes/SampleScene.unity" };
+        var path = $"artifacts/Build.il2cpp-dedicatedserver.exe";
+        string[] scenes = new string[] { "Assets/Scenes/SampleScene.unity" };
 
-        // Build player.
+        var buildPlayerOptions = new BuildPlayerOptions();
+
+        // Platform & Architecture
         buildPlayerOptions.target = BuildTarget.StandaloneWindows;
         buildPlayerOptions.subtarget = (int)StandaloneBuildSubtarget.Server;
-        BuildPipeline.BuildPlayer(levels, path, BuildTarget.StandaloneWindows, BuildOptions.None);
+
+        // IL2CPP
+        EnableIl2Cpp(NamedBuildTarget.Server);
+
+        // Path and Scenes
+        buildPlayerOptions.locationPathName = path;
+        buildPlayerOptions.scenes = scenes;
+
+        // Build
+        BuildPipeline.BuildPlayer(buildPlayerOptions);
     }
 
     [MenuItem("MyTools/Build Linux IL2CPP Player")]
-    public static void BuildGame()
+    public static void BuildLinuxIl2cppPlayer()
     {
-        var path = $"artifacts/{PlayerSettings.productName}.il2cpp-player";
-        string[] levels = new string[] { "Assets/Scenes/SampleScene.unity" };
+        var path = $"artifacts/Build.il2cpp-player.x86_64";
+        string[] scenes = new string[] { "Assets/Scenes/SampleScene.unity" };
 
-        // Build player.
+        var buildPlayerOptions = new BuildPlayerOptions();
+
+        // Platform & Architecture
         buildPlayerOptions.target = BuildTarget.StandaloneLinux64;
         buildPlayerOptions.subtarget = (int)StandaloneBuildSubtarget.Player;
-        BuildPipeline.BuildPlayer(levels, path, BuildTarget.StandaloneWindows, BuildOptions.None);
+
+        // IL2CPP
+        EnableIl2Cpp(NamedBuildTarget.Standalone);
+
+        // Path and Scenes
+        buildPlayerOptions.locationPathName = path;
+        buildPlayerOptions.scenes = scenes;
+
+        // Build
+        BuildPipeline.BuildPlayer(buildPlayerOptions);
     }
 
     [MenuItem("MyTools/Build Linux IL2CPP DedicatedServer")]
-    public static void BuildGame()
+    public static void BuildLinuxIl2cppServer()
     {
-        var path = $"artifacts/{PlayerSettings.productName}.il2cpp-dedicatedserver";
-        string[] levels = new string[] { "Assets/Scenes/SampleScene.unity" };
+        var path = $"artifacts/Build.il2cpp-dedicatedserver.x86_64";
+        string[] scenes = new string[] { "Assets/Scenes/SampleScene.unity" };
 
-        // Build player.
+        var buildPlayerOptions = new BuildPlayerOptions();
+
+        // Platform & Architecture
         buildPlayerOptions.target = BuildTarget.StandaloneLinux64;
         buildPlayerOptions.subtarget = (int)StandaloneBuildSubtarget.Server;
-        BuildPipeline.BuildPlayer(levels, path, BuildTarget.StandaloneWindows, BuildOptions.None);
+
+        // IL2CPP
+        EnableIl2Cpp(NamedBuildTarget.Server);
+
+        // Path and Scenes
+        buildPlayerOptions.locationPathName = path;
+        buildPlayerOptions.scenes = scenes;
+
+        // Build
+        BuildPipeline.BuildPlayer(buildPlayerOptions);
+    }
+
+    private static void EnableIl2Cpp(NamedBuildTarget buildTarget)
+    {
+        // IL2CPP Code Generation = Faster runtime
+        PlayerSettings.SetIl2CppCodeGeneration(buildTarget, Il2CppCodeGeneration.OptimizeSpeed);
     }
 }
